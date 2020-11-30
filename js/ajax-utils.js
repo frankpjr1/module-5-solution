4a41739d5c7d91a14842ffc2c91e6abfd96eefa5
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2020-11-29 21:45:59 -0500

    ajax update

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -1,16 +1,16 @@
 (function (global) {
 
-  
+  // Set up a namespace for our utility
   var ajaxUtils = {};
 
 
-  
+  // Returns an HTTP request object
   function getRequestObject() {
     if (window.XMLHttpRequest) {
       return (new XMLHttpRequest());
     }
     else if (window.ActiveXObject) {
-     
+      // For very old IE browsers (optional)
       return (new ActiveXObject("Microsoft.XMLHTTP"));
     }
     else {
@@ -20,6 +20,7 @@
   }
 
 
+  // Makes an Ajax GET request to 'requestUrl'
   ajaxUtils.sendGetRequest =
     function (requestUrl, responseHandler, isJsonResponse) {
       var request = getRequestObject();
@@ -30,18 +31,20 @@
             isJsonResponse);
         };
       request.open("GET", requestUrl, true);
-      request.send(null);
+      request.send(null); // for POST only
     };
 
 
-  
+  // Only calls user provided 'responseHandler'
+  // function if response is ready
+  // and not an error
   function handleResponse(request,
     responseHandler,
     isJsonResponse) {
     if ((request.readyState == 4) &&
       (request.status == 200)) {
 
-      
+      // Default to isJsonResponse = true
       if (isJsonResponse == undefined) {
         isJsonResponse = true;
       }
@@ -56,10 +59,9 @@
   }
 
 
-  /
+  // Expose utility to the global object
   global.$ajaxUtils = ajaxUtils;
 
 
 })(window);
-
 
